93d06a1ea1c34a3dc54a1a7ec0efc6bbf42cc730
carterjanet103-crypto <carterjanet103@gmail.com>
2026-01-31 13:41:14 +0100

    Create Transfers.tsx

---
+++ src/components/Transfers.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Transfers = () => {
+  return (
+    <div>
+      <h2>Transfers</h2>
+      <p>This is the Transfers component.</p>
+    </div>
+  );
+};
+
+export default Transfers;
