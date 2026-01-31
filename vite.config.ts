b4b71a25c7b30f6af26b389448105955d7f7ecbc
carterjanet103-crypto <carterjanet103@gmail.com>
2026-01-31 13:16:46 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // REMOVE the specific repo name for Vercel
-  base: '/', 
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    minify: 'esbuild',
-  },
-});
+})
